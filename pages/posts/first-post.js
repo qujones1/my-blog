e212fccdaa20b61eecff2d7b21fa9c71170667b1
qujones1 <32285137+qujones1@users.noreply.github.com>
2022-04-13 10:56:55 -0400

    Update first-post.js
    added return statement because I forgot it, like an idiot

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,12 +1,14 @@
 import Link from 'next/link'
 
 export default function FirstPost() {
-  <>
-  <h1>First Post</h1>
-  <h2>
-    <link href= "/">
-    <a> Return Home </a>
-    </link>
-  </h2>
-      
+  return (
+    <>
+      <h1>First Post</h1>
+      <h2>
+        <link href= "/">
+        <a> Return Home </a>
+        </link>
+      </h2>
+    </>
+   )   
 }

a358546c5c9a284c6c2420573982c230a9d14ae3
qujones1 <32285137+qujones1@users.noreply.github.com>
2022-04-13 11:26:36 -0400

    Update first-post.js
    added title

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,9 +1,13 @@
 import Link from 'next/link'
+import Head from 'next/head'
 
 export default function FirstPost() {
   return (
     <>
-      <h1>First Post</h1>
+      <Head>
+        <title>First Post</title>
+      </Head>
+      <h1> my first post</h1>
       <h2>
          <Link href="/">
           <a>Return Home</a>

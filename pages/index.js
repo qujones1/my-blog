5518d9cd6ecafae048e5f9d22d97ba9ae602ef6f
qujones1 <32285137+qujones1@users.noreply.github.com>
2022-04-13 13:47:51 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -37,7 +37,6 @@ export default function Home({ allPostsData }) {
                 <br />
                 {date}
               </li>
-              <br / >
             ))}
           </ul>
         </section>

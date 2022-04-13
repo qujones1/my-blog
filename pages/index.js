bf1269f04bdb9e4d86994b1bbc2528a3a912b9e8
qujones1 <32285137+qujones1@users.noreply.github.com>
2022-04-13 13:51:25 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -26,8 +26,8 @@ export default function Home({ allPostsData }) {
         </p>
       </section>
 
-      <section className={`${utilStyles.headingMd}`}>
-          <h2 className={utilStyles.headingLg}>Blog</h2>
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+          <h2 className={utilStyles.headingLg}>Blog Posts</h2>
           <ul className={utilStyles.list}>
             {allPostsData.map(({ id, date, title }) => (
               <li className={utilStyles.listItem} key={id}>

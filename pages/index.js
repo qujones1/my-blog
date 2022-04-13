d04e0b8a28e04046b8e8ef4b8371a6335bbce910
qujones1 <32285137+qujones1@users.noreply.github.com>
2022-04-13 10:54:16 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import Link from 'next/link'
 
 export default function Home() {
   return (
@@ -13,9 +14,9 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          <Link href="/posts/first-post">
+          <link href="/posts/first-post">
           <a>First post!</a>
-          </Link>
+          </link>
         </h1>
 
         <p className={styles.description}>

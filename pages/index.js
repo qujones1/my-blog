bb11aadf3647d03e261ef0db371d459cb658535e
qujones1 <32285137+qujones1@users.noreply.github.com>
2022-04-13 11:04:12 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -14,10 +14,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Read{' '}
-          <link href="/posts/first-post">
-          <a>First post!</a>
-          </link>
+         
         </h1>
 
         <p className={styles.description}>

06bbd026afb984f81435ebb8416e30f6fd1f8304
qujones1 <32285137+qujones1@users.noreply.github.com>
2022-04-14 08:43:14 -0400

    Update layout.js
    Made last name upper case

--- components/layout.js
+++ components/layout.js
@@ -4,7 +4,7 @@ import styles from './layout.module.css'
 import utilStyles from '../styles/utils.module.css'
 import Link from 'next/link'
 
-const name = 'Quentin jones'
+const name = 'Quentin Jones'
 export const siteTitle = 'Quentin Jones Blog'
 
 export default function Layout({ children, home }) {
